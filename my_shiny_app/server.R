db6831dccefd0f7f7acd4585e77d46449544c254
lboro-rdm <rdm@lboro.ac.uk>
2024-06-07 14:33:22 +0100

    added and commented out function to reset sliders

--- my_shiny_app/server.R
+++ my_shiny_app/server.R
@@ -123,6 +123,16 @@ function(input, output, session){
     paste("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.")
   })
   
-  
+  # # Function to reset sliders
+  # resetSliders <- function() {
+  #   updateSliderInput(session, "rotate", value = 0)
+  #   updateSliderInput(session, "blur", value = 0)
+  #   updateSliderInput(session, "implode", value = 0)
+  # }
+  # 
+  # # Observe tabPanel change
+  # observeEvent(input$image_viewer_tab_id, {
+  #   resetSliders()
+  # })
   
 }
