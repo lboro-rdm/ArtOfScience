64d81c5b8ed65a441b0c7a19f6b4564c804bdf9d
lboro-rdm <rdm@lboro.ac.uk>
2024-11-27 16:17:38 +0000

    updated all pictures

--- my_shiny_app/server.R
+++ my_shiny_app/server.R
@@ -4,13 +4,13 @@ function(input, output, session) {
     
     future_promise({
       
-      download.file("https://repository.lboro.ac.uk/ndownloader/files/44908975", 
+      download.file("https://repository.lboro.ac.uk/ndownloader/files/40017832", 
                     tempfile())
-      download.file("https://repository.lboro.ac.uk/ndownloader/files/44908990", 
+      download.file("https://repository.lboro.ac.uk/ndownloader/files/40017832", 
                     tempfile())
       download.file("https://repository.lboro.ac.uk/ndownloader/files/44908999", 
                     tempfile())
-      download.file("https://repository.lboro.ac.uk/ndownloader/files/44908981", 
+      download.file("https://repository.lboro.ac.uk/ndownloader/files/47255443", 
                     tempfile())
       download.file("https://repository.lboro.ac.uk/ndownloader/files/44908987", 
                     tempfile())
@@ -62,10 +62,16 @@ function(input, output, session) {
          width = '100%')
   })
   
-  output$text1 <- renderText({
-    paste("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.")
+  output$text1 <- renderUI({
+    HTML(paste0(
+      "An example of a design drawing based on an imaging experiment devised by Dr. Nicholas Holliday. HEK293 is a cell line used in the assay (experiment). In this assay a 100 nanomolar (a scientific measurement of con-centration) of the hunger hormone ghrelin was added to the live cells and imaged under the microscope to see the fluorescent receptors (SNAP labelling) removed from the cell mem-brane over a 15minute time-series. (",
+      a("Image and text source", href = "https://hdl.handle.net/2134/22357051"),
+      ")"
+                )
+    )
   })
-  # PICTURE 2
+  
+  # PICTURE 2 ----
   
   output$picture2 <- renderImage({
     
@@ -81,11 +87,18 @@ function(input, output, session) {
   })
   
   
-  output$text2 <- renderText({
-    paste("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.")
+  output$text2 <- renderUI({
+    HTML(paste0(
+      "The image is based on an assay to test how successfully the fluorescent receptors 
+(SNAP labelling) could be removed from the cell membrane over a 20minute time-series. The drawing is made from stills of a stereo-im-age movie made using Axio-Vision software. A four-layered design drawing was created and used as a template for the laser-cut image housed in a light-box and lit with LEDs. (",
+      a("Image and text source", href = "https://hdl.handle.net/2134/22357051"),
+      ")"
+               
+               )
+         )
   })
   
-  # PICTURE 3
+  # PICTURE 3 ----
   output$picture3 <- renderImage({
     
     modify_image()
@@ -98,9 +111,21 @@ function(input, output, session) {
          contentType = 'image/jpeg',
          width = '100%')
   })
-  output$text3 <- renderText({
-    paste("A Radiolarian imaged on a Zeiss Ultra Plus Scanning Electron Microscope (SEM)6. The drawing was constructed of simple hand drawn vector shapes (squares and rectangles) using the Adobe Illustrator programme. Drawing digitally is used to identify key details of the structural intricacy of the Radiolarian. The transcribed information was then copied, pasted and rotated over a five degree four times to visualise a drawn example of Photogrammetry")
+  output$text3 <- renderUI({
+    HTML(paste0(
+      "A Radiolarian imaged on a Zeiss Ultra Plus Scanning Electron Microscope (SEM). 
+     The drawing was constructed using simple hand-drawn vector shapes (squares and rectangles) 
+     in Adobe Illustrator. Digital drawing techniques were employed to highlight key details 
+     of the Radiolarian's structural intricacy. The transcribed information was copied, pasted, 
+     and rotated in five-degree increments four times, resulting in a drawn representation of 
+     Photogrammetry. (",
+      a("Text source", href = "https://www.taylorfrancis.com/chapters/edit/10.1201/b19744-12/visualization-scientific-image-data-art-data-jo-berry?context=ubx&refId=f6c81bfd-51da-437b-9731-bcbd55205773"), 
+      ", ",
+      a("Image source", href = "https://doi.org/10.17028/rd.lboro.25359211"),
+      ")"
+    ))
   })
+  
   
   # PICTURE 4
   output$picture4 <- renderImage({
@@ -115,8 +140,13 @@ function(input, output, session) {
          contentType = 'image/jpeg',
          width = '100%')
   })
-  output$text4 <- renderText({
-    paste("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.")
+  output$text4 <- renderUI({
+    HTML(paste0(
+      "Raw data mixed with software experiments and digital drawings of a cross-section of skin one with a hair follicle. (",
+      a("Image and text source", href = "https://doi.org/10.17028/rd.lboro.26106589"),
+      ")"
+        )
+    )
   })
   
   # PICTURE 5
@@ -134,8 +164,13 @@ function(input, output, session) {
     
   })
   
-  output$text5 <- renderText({
-    paste("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.")
+  output$text5 <- renderUI({
+    HTML(paste0(
+      "This drawing  is constructed of nine-layers. Raw data of human stem-cell-derived cardiomyocytes were copied at 100% transparency, then re-copied at 66% transparency, and offset. The original time-consuming design drawing was copied and pasted five times. Solid and dashed vector lines created the appearance of movement: 1] pink line was 1-point stroke weight, dashed; 2] light orange 1-point stroke weight, dashed; 3] orange 1-point stroke weight; 4] skin-tone 0.75-point stroke weight 5] brown 2- point stroke weight, dashed and 6] the nucleus outline coloured blue at 1-point stroke weight, dashed. At the same time, I asked myself why I wanted to make imperfect replications of human stem-cells-derived cardiomyocytes? Why was it important to develop a laborious method of tracing zoomed-in pixel detail, illuminated on-screen?  It was a time-consuming paradoxical method of drawing, yet I recognised the drawings I made were subtly re-mapping complexity. The drawings are exemplars of play and creativity, imbued with playfulness. I heavily invested in the creative process of drawing: observation, selection and action, in order to illuminate the potential of play to generate knowledge. The creative and intellectual game I played as I constructed digital drawings was immersive and cyclical. Through praxis, I was able to comprehend the mediating role of technology and how components arise as a result of this distinct re-mapping process. It was a practical example of how I gained a deeper understanding of how computer technology was shaping my experience of drawing, as human and non-human activity connected, and from it, I gained tacit knowledge (",
+      a("Image and text source", href = "https://doi.org/10.17028/rd.lboro.27907866"),
+      ")"
+      )
+    )
   
     })
 }
--- my_shiny_app/ui.R
+++ my_shiny_app/ui.R
@@ -1,9 +1,13 @@
 navbarPage("Art-Science Interplay", 
            tabPanel("Artist statement", 
-                    fluidPage(HTML("<p>I have been working as an artist for over thirty years. I am fascinated by light, colour, lasers, technology and science. My initial curiosity in how artists can work with scientists was piqued in 1998 when I became aware of the SciArt scheme. The ethos of the SciArt scheme encouraged me, as an artist, to seek out Life Scientists to collaborate with because the methods we employ to create images are connected. We both use laser technology (I make laser-cut lightboxes), which requires knowledge of light, optics, and computer visualisation methods and I am fascinated by how I can use scientific image data innovatively. As a result, since 2010, a central part of my practice has involved contributing to scientific research projects as one of the research team.</p>",
-
-"<p>I identified a gap in knowledge while working with scientists in labs. There was a lack of understanding between the two disciplines of approaches to imaging and its potential. I wanted to discover if and how an artist-researcher can contribute to new methods of interdisciplinary approaches in advanced imaging and microscopy through collaborative practice. Over the last ten years I have collaborated with Advanced imaging and Microscopy specialists, working with a network of internationally renowned core imaging laboratories in the field of Life Science. My aim is to dismantle silo mentalities so that artist-researchers can collaborate with scientists to create new representations, insights and behavioural change. I implemented a four-stage framework and protocol underpinned by the inclusion of play. Each element helped me negotiate and interpret art and science collaboration in new ways by extending art and scientific methods of visualisation. This led to non-standard representations, technological advancements, and better intellectual and visualisation skills, hence enhancing practice-based research through collaboration. Each element helped me negotiate and interpret art and science collaboration in new ways by extending art and scientific methods of visualisation. I advanced three methods of production: an introspective, digital drawing method using limited tools; data montages where data and documentary footage are explored; and experimental moving image work, integrating documentary film footage and sound.</p>",
-                              "<p><a href='https://www.joberry.co.uk/'>- Joanne Berry-Frith</a></p>"))),
+                    fluidPage(
+                              p("I have been working as an artist for over thirty years. I am fascinated by light, colour, lasers, technology and science. My initial curiosity in how artists can work with scientists was piqued in 1998 when I became aware of the SciArt scheme. The ethos of the SciArt scheme encouraged me, as an artist, to seek out Life Scientists to collaborate with because the methods we employ to create images are connected. We both use laser technology (I make laser-cut lightboxes), which requires knowledge of light, optics, and computer visualisation methods and I am fascinated by how I can use scientific image data innovatively. As a result, since 2010, a central part of my practice has involved contributing to scientific research projects as one of the research team."),
+                                p("I identified a gap in knowledge while working with scientists in labs. There was a lack of understanding between the two disciplines of approaches to imaging and its potential. I wanted to discover if and how an artist-researcher can contribute to new methods of interdisciplinary approaches in advanced imaging and microscopy through collaborative practice. Over the last ten years I have collaborated with Advanced imaging and Microscopy specialists, working with a network of internationally renowned core imaging laboratories in the field of Life Science. My aim is to dismantle silo mentalities so that artist-researchers can collaborate with scientists to create new representations, insights and behavioural change. I implemented a four-stage framework and protocol underpinned by the inclusion of play. Each element helped me negotiate and interpret art and science collaboration in new ways by extending art and scientific methods of visualisation. This led to non-standard representations, technological advancements, and better intellectual and visualisation skills, hence enhancing practice-based research through collaboration. Each element helped me negotiate and interpret art and science collaboration in new ways by extending art and scientific methods of visualisation. I advanced three methods of production: an introspective, digital drawing method using limited tools; data montages where data and documentary footage are explored; and experimental moving image work, integrating documentary film footage and sound."),
+                              div(
+                                a("- Joanne Berry-Frith", href = "https://www.joberry.co.uk/"), 
+                                style = "text-align: right;"
+                              )
+)),
            tabPanel(
              "Exhibition",
              fluidPage(HTML("Enjoy playing scientist, by changing the sliders on the left and seeing the images in different rotation, focus and implosion.<p></p>"),
@@ -32,27 +36,27 @@ navbarPage("Art-Science Interplay",
                    id = "image_viewer_tab_id",
                    tabPanel("Image 1", 
                             withSpinner(imageOutput("picture1", height = "auto"), type = 5),
-                            withSpinner(textOutput("text1"), type = 5), 
+                            withSpinner(uiOutput("text1"), type = 5), 
                             value = "image_1"
                    ),
                    tabPanel("Image 2",
                             withSpinner(imageOutput("picture2", height = "auto"), type = 5),
-                            withSpinner(textOutput("text2"), type = 5), 
+                            withSpinner(uiOutput("text2"), type = 5), 
                             value = "image_2"
                    ),
                    tabPanel("Image 3",
                             withSpinner(imageOutput("picture3", height = "auto"), type = 5),
-                            withSpinner(textOutput("text3"), type = 5), 
+                            withSpinner(uiOutput("text3"), type = 5), 
                             value = "image_3"
                    ),
                    tabPanel("Image 4",
                             withSpinner(imageOutput("picture4", height = "auto"), type = 5),
-                            withSpinner(textOutput("text4"), type = 5), 
+                            withSpinner(uiOutput("text4"), type = 5), 
                             value = "image_4"
                    ),
                    tabPanel("Image 5",
                             withSpinner(imageOutput("picture5", height = "auto"), type = 5),
-                            withSpinner(textOutput("text5"), type = 5), 
+                            withSpinner(uiOutput("text5"), type = 5), 
                             value = "image_5"
                    )
                  ))
